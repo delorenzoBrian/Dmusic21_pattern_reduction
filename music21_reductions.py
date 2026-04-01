1ba2f70ec67b8d10280c018e4643426bd70daa29
SamFranceseTCNJ <frances4@tcnj.edu>
2026-04-01 14:59:18 -0400

    implemented new algorithm for detecting patterns and making reductions

--- music21_reductions.py
+++ music21_reductions.py
@@ -3,6 +3,7 @@ import os
 import music21
 from importlib import util
 from collections import Counter
+from collections import defaultdict
 
 # Verify that music21 is installed
 print("Checking if 'music21' is installed...")
@@ -26,8 +27,10 @@ print("Running converter...\n")
 # Parse the file using music21
 try:
     song = music21.converter.parse(file)
-    song = song.chordify()  # Chordify the song
+    #song = song.chordify()  # Chordify the song
+    num_parts = len(song.parts)
     print("File parsed and chordified successfully.")
+    print(f"Found {num_parts} parts.\n")
 except Exception as e:
     print(f"Error parsing the file: {e}")
     exit()
@@ -134,6 +137,7 @@ def prioritize_melody(reduced_notes, repeated_patterns, max_voices):
     return prioritized_notes[:max_voices]  # Ensure the list never exceeds max_voices
 
 # Function to reduce a chord to a specific number of voices
+# skipping prioritize melody for now since this is already being called on melody notes
 def reduce_chord(chord_obj, max_voices, repeated_patterns):
     notes = chord_obj.notes
     reduced_notes = []
@@ -147,10 +151,28 @@ def reduce_chord(chord_obj, max_voices, repeated_patterns):
         reduced_notes = filtered_notes
 
     # If the chord is too short (i.e., fewer notes than max_voices), only use the original notes
-    return music21.chord.Chord(reduced_notes)
+    return music21.chord.Chord(filtered_notes[:max_voices]) # Ensure the chord never exceeds max_voices
+
+def add_notes(old_score, max_voices, chord_obj=music21.chord.Chord()):
+    # Get the original notes from the chord
+    original_notes = chord_obj.notes
+    added_notes = list(original_notes)  # Start with the original notes
+    old_score_chord = old_score.getElementsByOffset(chord_obj.offset)
+    if len(old_score_chord) > 0 and old_score_chord[0].isChord:
+        old_score_chord = old_score_chord[0] # Get the first element at that offset, which should be the chord in the original score
+
+    # add notes from the original score back in until we hit the max voices limit
+    if old_score_chord and old_score_chord.isChord:
+        for note in old_score_chord.notes:
+            if note not in added_notes:
+                added_notes.append(note)
+                if len(added_notes) >= max_voices:
+                    break
+
+    return music21.chord.Chord(added_notes)
 
 # Function to reduce a music21 score to a specific number of voices (without adding extra notes)
-def reduce_score(score_obj, max_voices, repeated_patterns):
+def add_patterns(score_obj, repeated_patterns, chunk_size=5):
     reduced_stream = music21.stream.Part()
 
     # Preserve the time signature and tempo
@@ -164,44 +186,111 @@ def reduce_score(score_obj, max_voices, repeated_patterns):
         reduced_stream.append(tempo_mark[0])  
 
     # Loop through the original score
-    for element in score_obj.flatten().notesAndRests:
-        if isinstance(element, music21.chord.Chord):
-            reduced_chord = reduce_chord(element, max_voices, repeated_patterns)
-            # Only append if the reduced chord has notes (no artificial additions)
-            if reduced_chord.notes:
-                reduced_stream.append(reduced_chord)
-            else:
-                # If no notes are left in the chord, append rests to meet the limit
-                for _ in range(max_voices - len(reduced_chord.notes)):
-                    reduced_stream.append(music21.note.Rest(quarterLength=element.quarterLength))
+    notes_and_rests = score_obj.flatten().notesAndRests
+    for i in range(len(notes_and_rests)-chunk_size+1): 
+        element = notes_and_rests[i]
+        noteList = get_chunk(notes_and_rests, i, chunk_size)
+        pattern = get_chord_pattern(noteList)
+        if pattern in repeated_patterns:
+            print(f"Found repeated pattern at offset {element.offset}: {pattern} (occurs {repeated_patterns[pattern]} times)")
+            #insert the pattern into the reduced stream
+            for j in notes_and_rests[i:i+chunk_size]:
+                #check for duplicates
+                existing_element = reduced_stream.flatten().getElementsByOffset(j.offset)
+                if len(existing_element) == 0:
+                    reduced_stream.insert(j.offset, j)  # Insert the original note or chord at the correct offset. Might cause overlap issues.
         else:
-            reduced_stream.append(element)  # Append rests or other elements
+            # if there's no pattern, just insert a rest as a placeholder to maintain timing
+            # first check if there's already a note or rest at this offset in the reduced stream to avoid overwriting it
+            existing_element = reduced_stream.flatten().getElementsByOffset(element.offset)
+            if len(existing_element) == 0:
+                rest = music21.note.Rest(quarterLength=element.quarterLength)
+                reduced_stream.insert(element.offset, rest)
+
+    # Loop through the last few elements that weren't included in the chunk processing and add rests to maintain timing
+    for i in range(len(notes_and_rests)-chunk_size+1, len(notes_and_rests)):
+        element = notes_and_rests[i]
+        existing_element = reduced_stream.flatten().getElementsByOffset(element.offset)
+        if len(existing_element) == 0:
+            rest = music21.note.Rest(quarterLength=element.quarterLength)
+            reduced_stream.insert(element.offset, rest)
 
     return reduced_stream
 
+def reduce_score(score_obj, max_voices, repeated_patterns, reduced_stream):
+    old_score = score_obj.flatten().notesAndRests
+    # Loop through the old score and reduce or add notes to the new score as necessary
+    for old_element in old_score.flatten().notesAndRests:
+        element = reduced_stream.getElementsByOffset(old_element.offset)
+        if len(element) == 0:
+            element = old_element
+        else:
+            element = element[0]
+        offset = element.offset
+
+        if isinstance(element, music21.chord.Chord):
+            if len(element.notes) > max_voices:
+                reduced_chord = reduce_chord(element, max_voices, repeated_patterns) # reduce the chord to the max voices limit
+            elif len(element.notes) < max_voices:
+                reduced_chord = add_notes(old_score, max_voices, element) # add notes from the orignal score back in until we hit the max voices limit
+            else:
+                reduced_chord = element
+
+            # Only append if the reduced chord has notes (no artificial additions)
+            if reduced_chord.notes:
+                reduced_stream.remove(element) # Remove the original chord
+                reduced_stream.insertAndShift(offset, reduced_chord) # Insert the reduced chord at the same offset. might cause overlap issues
+        elif old_element.isChord:
+            # check if there's a chord at this offset in the original score that we can add notes back from
+            if len(old_element.notes) > max_voices:
+                added_chord = reduce_chord(old_element, max_voices, repeated_patterns) # reduce the chord to the max voices limit
+            elif len(old_element.notes) < max_voices: #probably don't need this branch
+                added_chord = add_notes(old_score, max_voices, old_element)
+            else:
+                added_chord = old_element
+            reduced_stream.remove(element) # Remove the original rest
+            reduced_stream.insertAndShift(offset, added_chord) # Insert the added chord at the same offset. might cause overlap issues
+            print(f"Added notes back at offset {offset} based on original score chord: {added_chord.pitchedCommonName}")
+
+    return reduced_stream
+
+def get_chord_pattern(notes):
+    distances = []
+    noteLengths = []
+    for i in range(len(notes)):
+        if i < len(notes) - 1:
+            distances.append(notes[i+1].pitch.midi - notes[i].pitch.midi)
+        noteLengths.append(notes[i].duration.type)
+
+    return tuple(distances), tuple(noteLengths) # convert to tuple for hashability
+
+def get_chunk(notes, i, chunk_size=5):
+    chunk = []
+    for j in range(i, i+chunk_size):
+        element = notes[j]
+        if element.isNote:
+            chunk.append(element)
+        elif element.isChord:
+            chunk.append(music21.note.Note(element.root())) # only considering root for now, might expand to the whole chord or just use the highest note in the future
+    return chunk
 
 # Function to find repeated patterns of notes in the score
 def find_repeated_patterns(score_obj, min_length=5):
-    patterns = []
+    flat = score_obj.flatten().notesAndRests
+    notesAndChords = [n for n in flat if n.isNote or n.isChord]
     
-    # Flatten the score to analyze notes
-    notes = [n for n in score_obj.flatten().notes if n.isNote]
-    
-    for i in range(len(notes)):
-        for j in range(i + min_length, len(notes) + 1):
-            pattern = tuple(n.nameWithOctave for n in notes[i:j])  # Create a tuple of note names
-            patterns.append(pattern)
-
-    # Count occurrences of each pattern
-    pattern_counts = Counter(patterns)
-    
-    # Filter patterns to include only those that occur more than once
-    repeated_patterns = {p: count for p, count in pattern_counts.items() if count > 1}
-    
-    return repeated_patterns
+    for i in range(len(notesAndChords)-min_length+1):
+        noteList = get_chunk(notesAndChords, i)
+        repeated_patterns[get_chord_pattern(noteList)] += 1
 
 # Main processing of chords
-repeated_patterns = find_repeated_patterns(song)
+repeated_patterns = defaultdict(int)
+for part in song.parts:
+    part = part.chordify()
+    find_repeated_patterns(part)
+# Filter out patterns that only occur once
+repeated_patterns = {pattern: count for pattern, count in repeated_patterns.items() if count > 1}
+print(f"Identified {len(repeated_patterns)} sets of repeated patterns across the parts.")
 
 if limit <= 1:
     part = music21.stream.Part()
@@ -259,16 +348,21 @@ if limit <= 1:
             part.append(note)
 
 else:
-    reduced_score = reduce_score(song, limit, repeated_patterns)
+    part_idx = 0
     new_score = music21.stream.Score(id='main_score')
-    new_score.append(reduced_score)  # Append the reduced score directly
+    for part in song.parts:
+        reduced_part = add_patterns(part, repeated_patterns)
+        new_score.insert(0, reduced_part)  # Insert the reduced part directly into the score
+        part_idx += 1
+    new_score = new_score.chordify()  # Chordify the new score after adding patterns
+    new_score = reduce_score(song.chordify(), limit, repeated_patterns, new_score)
 
 # Apply dynamics to each part
 for dynamic in dynamics:
     new_score.insert(dynamic.offset, dynamic)
 
 # Output new file
-output = new_score.chordify()
+output = new_score
 output.insert(0, music21.metadata.Metadata())
 output.metadata.title = f"{song_title} - Reduced to {limit} voices"
 output.write('musicxml.pdf', fp='MuTest')
